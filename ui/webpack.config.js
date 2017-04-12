5f4c27fc708049d5e65bb0a721d4c17a51371e6d
Steve Haslam <araqnid@googlemail.com>
2017-04-12 22:45:49 +0100

    Upgrade to Webpack 2, React 15

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,48 +1,49 @@
 var webpack = require("webpack");
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var assetsdir = __dirname + "/www/assets";
+var assetsdir = path.resolve(__dirname, "www/assets");
 
-var config = {
+module.exports = {
     context: assetsdir + "/js",
-    entry: {
-        main: "app/pages/main",
-        status: "app/pages/status"
-    },
+    entry: "app/pages/main",
     output: {
         path: assetsdir + "/_pack",
         filename: "[name].js"
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components|js\/lib\/)/,
-                loader: 'babel-loader?presets[]=react&presets[]=es2015'
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [['es2015', { modules: false }], 'react']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
             },
             {
                 test: /jquery\.ba-hashchange\.js/,
-                loader: "imports?this=>global"
-            },
-            {
-                test: /galleria\.js/,
-                loader: "imports?this=>global"
-            },
-            {
-                test: /galleria\.classic\.js/,
-                loader: "imports?Galleria=galleria"
+                loader: "imports-loader?this=>global"
             }
         ]
     },
     resolve: {
-        root: [assetsdir + "/js/lib", assetsdir],
-        extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx'],
+        // root: [assetsdir + "/js/lib", assetsdir],
+        extensions: ['.webpack.js', '.web.js', '.js', '.jsx'],
         alias: {
             "app": assetsdir + "/js/app",
             "lodash": "lodash/index.js",
             "jquery": "jquery/dist/jquery.js",
             "bootstrap": "bootstrap/dist/js/bootstrap.js",
             "react$": "react/react.js",
-            "react-addons-css-transition-group": "react-addons-css-transition-group/index.js"
+            "react-addons-css-transition-group": "react-addons-css-transition-group/index.js",
+            "jquery.ba-hashchange": assetsdir + "/js/lib/jquery.ba-hashchange.js"
         }
     },
     plugins: [
@@ -53,15 +54,7 @@ var config = {
             "window.jQuery": "jquery"
         })
     ]
-}
-
-if (process.env.NODE_ENV === "production") {
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin({ sourceMap: false, compress: { warnings: false } }));
-    config.plugins.push(new webpack.optimize.OccurenceOrderPlugin());
-    config.plugins.push(new webpack.DefinePlugin({ "process.env": { NODE_ENV: JSON.stringify("production") } }));
-}
-
-module.exports = config;
+};
 
 // Local Variables:
 // compile-command: "nodejs node_modules/webpack/bin/webpack.js -d    --display-modules"
