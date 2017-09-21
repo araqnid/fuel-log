a491bded1c1aaa6e857a5e56ef941143009d43cb
Steve Haslam <araqnid@googlemail.com>
2017-09-21 02:53:04 +0100

    Various dependency updates

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: [['es2015', { modules: false }], 'react']
+                        presets: [['env', { modules: false }], 'react']
                     }
                 }
             },
