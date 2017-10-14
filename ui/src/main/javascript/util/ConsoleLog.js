3f6b509453bdcd9a144db88b1d492e1256c53859
Steve Haslam <araqnid@googlemail.com>
2017-10-15 00:53:47 +0100

    Silence console info messages in production

--- ui/src/main/javascript/util/ConsoleLog.js
+++ ui/src/main/javascript/util/ConsoleLog.js
@@ -1,14 +1,18 @@
 export function logFactory(prefix, delegate = console) {
     return {
         info: function() {
-            const params = Array.prototype.slice.call(arguments, 1, arguments.length);
-            params.unshift(prefix + ": " + arguments[0]);
-            delegate.log.apply(delegate, params);
+            if (process.env.NODE_ENV !== "production") {
+                const params = Array.prototype.slice.call(arguments, 1, arguments.length);
+                params.unshift(prefix + ": " + arguments[0]);
+                delegate.log.apply(delegate, params);
+            }
         },
         log: function() {
-            const params = Array.prototype.slice.call(arguments, 1, arguments.length);
-            params.unshift(prefix + ": " + arguments[0]);
-            delegate.log.apply(delegate, params);
+            if (process.env.NODE_ENV !== "production") {
+                const params = Array.prototype.slice.call(arguments, 1, arguments.length);
+                params.unshift(prefix + ": " + arguments[0]);
+                delegate.log.apply(delegate, params);
+            }
         },
         warn: function() {
             const params = Array.prototype.slice.call(arguments, 1, arguments.length);
