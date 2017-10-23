1736018cdd2f8e0d46660488277f870da2820ccd
Steve Haslam <araqnid@googlemail.com>
2017-10-23 20:48:22 +0100

    Use native forEach when starting stores, and expose them as a window variable in dev

--- ui/src/main/javascript/main.js
+++ ui/src/main/javascript/main.js
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./Root";
-import _ from "lodash";
 import * as stores from "./stores";
 
-_.forEach(stores, store => {
+Object.values(stores).forEach(store => {
     if (process.env.NODE_ENV !== "production") console.log("starting store", store);
     store.begin();
 });
+
+if (process.env.NODE_ENV !== "production") {
+    console.log("stores available as STORES");
+    window.STORES = stores;
+}
 
 ReactDOM.render(React.createElement(Root), document.getElementById("component.Root"));
