ad04a63ed14a82a5d8cd6fbfce26407c3dde82f9
Steve Haslam <araqnid@googlemail.com>
2017-12-02 01:23:42 +0000

    Try to get app-status JAR registered as a task input

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -4,18 +4,21 @@ plugins {
 
 tasks {
     "webpack" {
+        inputs.files(rootProject.configurations["compile"].fileCollection { dep -> dep.group == "org.araqnid" && dep.name == "app-status" })
+
         doFirst {
             val cfg = rootProject.configurations["compile"].copy()
             cfg.isTransitive = false
             val jarFile = cfg.files { dep -> dep.group == "org.araqnid" && dep.name == "app-status" }.single()
+            logger.info("Include app-status UI from $jarFile")
             val tmpDir = File(buildDir, "appStatus")
-            copy {
+            sync {
                 into(tmpDir)
                 from(zipTree(jarFile))
                 include("org/araqnid/appstatus/site/**")
             }
-            copy {
-                into(file("build/site/_status"))
+            sync {
+                into(File(buildDir, "site/_status"))
                 from(File(tmpDir, "org/araqnid/appstatus/site"))
             }
         }
