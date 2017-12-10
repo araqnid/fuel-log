ab127b8fa7133e579464c0172fa7b49566153b28
Steve Haslam <araqnid@googlemail.com>
2017-12-10 18:46:53 +0000

    Extend CoroutineDispatcher rather than ContinuationInterceptor directly

--- src/main/kotlin/org/araqnid/fuellog/ResteasyAsync.kt
+++ src/main/kotlin/org/araqnid/fuellog/ResteasyAsync.kt
@@ -3,9 +3,7 @@ package org.araqnid.fuellog
 import kotlinx.coroutines.experimental.CoroutineDispatcher
 import kotlinx.coroutines.experimental.DefaultDispatcher
 import org.jboss.resteasy.spi.ResteasyProviderFactory
-import kotlin.coroutines.experimental.AbstractCoroutineContextElement
-import kotlin.coroutines.experimental.Continuation
-import kotlin.coroutines.experimental.ContinuationInterceptor
+import kotlin.coroutines.experimental.CoroutineContext
 
 internal inline fun <T> withThreadContextData(data: Map<Class<*>, Any>, block: () -> T): T {
     ResteasyProviderFactory.pushContextDataMap(data)
@@ -16,25 +14,15 @@ internal inline fun <T> withThreadContextData(data: Map<Class<*>, Any>, block: (
     }
 }
 
-private class ResteasyAsyncContinuation<T>(private val data: Map<Class<*>, Any>, val cont: Continuation<T>) : Continuation<T> by cont {
-    override fun resume(value: T) {
-        withThreadContextData(data) {
-            cont.resume(value)
-        }
-    }
-
-    override fun resumeWithException(exception: Throwable) {
-        withThreadContextData(data) {
-            cont.resumeWithException(exception)
-        }
-    }
-}
-
 class ResteasyAsync(
         private val data: Map<Class<*>, Any> = ResteasyProviderFactory.getContextDataMap(),
         private val next: CoroutineDispatcher = DefaultDispatcher
-) : AbstractCoroutineContextElement(ContinuationInterceptor), ContinuationInterceptor {
-    override fun <T> interceptContinuation(continuation: Continuation<T>): Continuation<T> {
-        return next.interceptContinuation(ResteasyAsyncContinuation(data, continuation))
+) : CoroutineDispatcher() {
+    override fun dispatch(context: CoroutineContext, block: Runnable) {
+        next.dispatch(context, Runnable {
+            withThreadContextData(data) {
+                block.run()
+            }
+        })
     }
 }
