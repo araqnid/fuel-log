88fba31ca0a44fc830928d4432b30c2201ca4c2f
Steve Haslam <araqnid@googlemail.com>
2017-12-10 21:07:17 +0000

    Use async methods in IdentityStore

--- ui/src/main/javascript/stores/IdentityStore.js
+++ ui/src/main/javascript/stores/IdentityStore.js
@@ -56,62 +56,61 @@ export default class IdentityStore {
         this._realmProviders.FACEBOOK.onAvailable(() => this.dispatch({ type: "IdentityStore/facebookAvailable", payload: true }));
         this._realmProviders.GOOGLE.onUserUpdate(userInfo => this.dispatch({ type: "IdentityStore/localUserIdentity", payload: userInfo }));
         Object.values(this._realmProviders).forEach(p => p.start());
-        axios.get("_api/user/identity")
-            .then(({data: {user_info: userInfo}}) => {
-                if (userInfo) {
-                    const realm = this._realmProviders[userInfo.realm];
-                    if (realm) {
-                        log.info("last known user identity is in known realm, confirm it", userInfo);
-                        return realm.confirmUser(userInfo).then(confirmed => {
-                            if (confirmed) {
-                                log.info("User details confirmed");
-                                return confirmed;
-                            }
-                            else {
-                                log.info("User details not confirmed");
-                                return axios.delete("_api/user/identity").then(() => null);
-                            }
-                        });
-                    }
-                    else {
-                        log.warn("last known user identity is in unknown realm");
-                        return axios.delete("_api/user/identity").then(() => null);
-                    }
-                }
-                else {
-                    log.info("Asking realm providers to probe for identity");
-                    const names = Object.keys(this._realmProviders);
-                    return Promise.all(names.map(name => this._realmProviders[name].probe()))
-                        .then(values => {
-                            log.info("Collected probe results", values);
-                            const result = {};
-                            names.forEach((name, index) => {
-                                result[name] = values[index];
-                            });
-                            return result;
-                        })
-                        .then(result => {
-                            log.info("Assembled probe results", result);
-                            if (result.GOOGLE) {
-                                return this._realmProviders.GOOGLE.autoLogin();
-                            }
-                            else if (result.FACEBOOK) {
-                                return this._realmProviders.FACEBOOK.autoLogin();
-                            }
-                            else {
-                                return null;
-                            }
-                        });
-                }
-            })
-            .then(userInfo => {
-                log.info("final user: ", userInfo);
-                this.dispatch({ type: "IdentityStore/localUserIdentity", payload: userInfo });
-            })
+        this._launch();
     }
 
     stop() {
         Object.values(this._realmProviders).forEach(p => p.stop());
+    }
+
+    async _launch() {
+        let {data: {user_info: userInfo}} = await axios.get("_api/user/identity");
+        if (userInfo) {
+            const realm = this._realmProviders[userInfo.realm];
+            if (realm) {
+                log.info("last known user identity is in known realm, confirm it", userInfo);
+                const confirmed = await realm.confirmUser(userInfo);
+                if (confirmed) {
+                    log.info("User details confirmed");
+                    userInfo = confirmed;
+                }
+                else {
+                    log.info("User details not confirmed");
+                    await axios.delete("_api/user/identity");
+                    userInfo = null;
+                }
+            }
+            else {
+                log.warn("last known user identity is in unknown realm");
+                await axios.delete("_api/user/identity");
+                userInfo = null;
+            }
+        }
+        else {
+            log.info("Asking realm providers to probe for identity");
+            const names = Object.keys(this._realmProviders);
+            const values = await Promise.all(names.map(name => this._realmProviders[name].probe()));
+
+            log.info("Collected probe results", values);
+            const result = {};
+            names.forEach((name, index) => {
+                result[name] = values[index];
+            });
+
+            log.info("Assembled probe results", result);
+            if (result.GOOGLE) {
+                userInfo = this._realmProviders.GOOGLE.autoLogin();
+            }
+            else if (result.FACEBOOK) {
+                userInfo = this._realmProviders.FACEBOOK.autoLogin();
+            }
+            else {
+                userInfo = null;
+            }
+        }
+
+        log.info("final user: ", userInfo);
+        this.dispatch({ type: "IdentityStore/localUserIdentity", payload: userInfo });
     }
 
     get _reduxUserInfo() {
@@ -127,22 +126,19 @@ export default class IdentityStore {
         return this._signIn(this._realmProviders.FACEBOOK);
     }
 
-    signOut() {
+    async signOut() {
         const userInfo = this._reduxUserInfo;
         if (!userInfo) return;
         log.info("Sign out", userInfo);
-        return this._realmProviders[userInfo.realm].signOut()
-            .then(() => axios.delete("/_api/user/identity"))
-            .then(() => {
-                this.dispatch({ type: "IdentityStore/localUserIdentity", payload: null });
-            });
+        await this._realmProviders[userInfo.realm].signOut();
+        await axios.delete("/_api/user/identity");
+        this.dispatch({ type: "IdentityStore/localUserIdentity", payload: null });
     }
 
-    _signIn(realm) {
-        return realm.signIn().then(userInfo => {
-            log.info("completed realm sign in", userInfo);
-            this.dispatch({ type: "IdentityStore/localUserIdentity", payload: userInfo });
-        });
+    async _signIn(realm) {
+        const userInfo = await realm.signIn();
+        log.info("completed realm sign in", userInfo);
+        this.dispatch({ type: "IdentityStore/localUserIdentity", payload: userInfo });
     }
 
     dispatch(action) {
