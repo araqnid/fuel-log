8c5a3b22f1a10ced2a4cfe9ea481f1b14aebaa8d
Steve Haslam <steve.haslam@timgroup.com>
2017-12-15 16:31:08 +0000

    Simpler initialisers for extra properties that are just version strings

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,11 +15,11 @@ application {
     mainClassName = "org.araqnid.fuellog.boot.Main"
 }
 
-val jettyVersion by extra { "9.4.8.v20171121" }
-val jacksonVersion by extra { "2.9.1" }
-val resteasyVersion by extra { "3.1.4.Final" }
-val guiceVersion by extra { "4.1.0" }
-val guavaVersion by extra { "23.5-jre" }
+val jettyVersion by extra("9.4.8.v20171121")
+val jacksonVersion by extra("2.9.1")
+val resteasyVersion by extra("3.1.4.Final")
+val guiceVersion by extra("4.1.0")
+val guavaVersion by extra("23.5-jre")
 
 val gitVersion by extra {
     val capture = ByteArrayOutputStream()
