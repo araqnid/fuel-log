4aea0771d761f6c5973ee80204307ffb1500acea
Steve Haslam <araqnid@googlemail.com>
2017-12-15 21:47:21 +0000

    Stick to using compile rather than implementation configuration
    I need to figure out the ramifications of actually using implementation,
    and it seems to be something that should be pushed from the libs rather
    than back from the apps

--- test-utils/build.gradle.kts
+++ test-utils/build.gradle.kts
@@ -10,6 +10,6 @@ dependencies {
     compile("com.natpryce:hamkrest:1.4.2.2")
     compile("com.google.guava:guava:$guavaVersion")
     compile("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
-    implementation(kotlin("stdlib-jdk8"))
-    implementation(kotlin("reflect"))
+    compile(kotlin("stdlib-jdk8"))
+    compile(kotlin("reflect"))
 }
