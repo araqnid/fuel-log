cfbfd97a879dc2d73c7b1b07ffa30457ae93544d
Steve Haslam <steve.haslam@timgroup.com>
2018-03-02 11:52:18 +0000

    Kotlin 1.2.30

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ import java.security.MessageDigest
 
 plugins {
     application
-    kotlin("jvm") version "1.2.21"
+    kotlin("jvm") version "1.2.30"
     `java-library`
     id("com.timgroup.webpack") version "1.0.12" apply false
 }
