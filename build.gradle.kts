6e86a387e203d4c9d6cb7e94c43478ee016ddddd
Steve Haslam <araqnid@googlemail.com>
2020-02-08 14:30:16 +0000

    Gradle 6.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.3.50" apply false
-    id("com.timgroup.webpack") version "1.0.50" apply false
+    kotlin("jvm") version "1.3.61" apply false
+    id("com.timgroup.webpack") version "1.0.63" apply false
 }
 
 LibraryVersions.toMap().forEach { (name, value) ->
