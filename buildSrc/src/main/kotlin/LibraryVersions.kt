1e763fc1ae191172f46ad212b930e4943b338732
Steve Haslam <araqnid@googlemail.com>
2020-02-15 21:08:23 +0000

    Use Jackson 2.10.2

--- buildSrc/src/main/kotlin/LibraryVersions.kt
+++ buildSrc/src/main/kotlin/LibraryVersions.kt
@@ -2,7 +2,7 @@ import kotlin.reflect.full.memberProperties
 
 object LibraryVersions {
     const val jetty = "9.4.26.v20200117"
-    const val jackson = "2.9.9"
+    const val jackson = "2.10.2"
     const val resteasy = "3.1.4.Final"
     const val guice = "4.2.2"
     const val guava = "28.1-jre"
