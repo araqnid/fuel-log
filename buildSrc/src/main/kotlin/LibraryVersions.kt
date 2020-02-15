aa891426c160ce9f8eb67ce0f70cfd317201d8f4
Steve Haslam <araqnid@googlemail.com>
2020-02-15 23:14:25 +0000

    Update kotlinCoroutines version

--- buildSrc/src/main/kotlin/LibraryVersions.kt
+++ buildSrc/src/main/kotlin/LibraryVersions.kt
@@ -6,7 +6,7 @@ object LibraryVersions {
     const val resteasy = "3.1.4.Final"
     const val guice = "4.2.2"
     const val guava = "28.1-jre"
-    const val kotlinCoroutines = "1.3.1"
+    const val kotlinCoroutines = "1.3.3"
     const val appStatus = "0.1.2"
     const val slf4j = "1.7.30"
 
